873e6a2ae3762991a36fac968956bc8646791495
Nikita <kbshnik2015@gmail.com>
2021-02-12 16:38:53 +0400

    # added filtering of specifications on the "show specifications" page that are already connected at the customer # removed information that should not be available to the customer # fixed errors

--- MiniOPF/src/main/java/controller/managers/PayDayManager.java
+++ MiniOPF/src/main/java/controller/managers/PayDayManager.java
@@ -13,24 +13,29 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@SuppressWarnings ("FieldCanBeLocal")
 public class PayDayManager extends Thread
 {
-    private final int ONE_MINUTE = 60000;
+    private final int ONE_MINUTE_IN_MILLI_SEC = 60000;
     private final long MONTH_IN_MILLI_SEC = 2592000000L;
+
     @SneakyThrows
     @Override
     public void run()
     {
-
         Model model = Model.getInstance();
         Controller controller = new Controller();
-        List<Service> services = new ArrayList<>(model.getServices().values());
+        List<Service> services = new ArrayList<>(model.getServices()
+                .values());
         while (true)
         {
             Date currentDate = new Date();
             for (Service service : services)
             {
-                if (service.getPayDay().before(currentDate) && (ServiceStatus.ACTIVE.equals(service.getServiceStatus()) || ServiceStatus.PAY_MONEY_SUSPENDED.equals(service.getServiceStatus())))
+                boolean isPayDayComeForActiveService = service.getPayDay()
+                        .before(currentDate) && ServiceStatus.ACTIVE.equals(service.getServiceStatus());
+                if (isPayDayComeForActiveService ||
+                        ServiceStatus.PAY_MONEY_SUSPENDED.equals(service.getServiceStatus()))
                 {
                     Customer customer = model.getCustomer(service.getCustomerLogin());
                     Specification serviceSpec = model.getSpecification(service.getSpecificationId());
@@ -39,7 +44,7 @@ public class PayDayManager extends Thread
                     {
                         controller.changeBalanceOn(customer.getLogin(), serviceSpec.getPrice() * (-1));
                         service.setServiceStatus(ServiceStatus.ACTIVE);
-                        service.setPayDay(new Date (service.getPayDay().getTime() + MONTH_IN_MILLI_SEC));
+                        service.setPayDay(new Date(System.currentTimeMillis() + MONTH_IN_MILLI_SEC));
                     }
                     else
                     {
@@ -47,13 +52,18 @@ public class PayDayManager extends Thread
                     }
                 }
             }
-            try
-            {
-                sleep(ONE_MINUTE);
-            }catch (InterruptedException e){
-                //We don't need a notification about an interruption of this daemon thread
-            }
+            sleepForOneMinute();
+        }
+    }
 
+    private void sleepForOneMinute()
+    {
+        try
+        {
+            sleep(ONE_MINUTE_IN_MILLI_SEC);
+        } catch (InterruptedException e)
+        {
+            //We don't need a notification about an interruption of this daemon thread
         }
     }
 }
