bb8dc8da285d7683dabe69f2d45ec1af2b2e7efc
nikitas666 <nikita410777@mail.ru>
2021-05-14 19:16:13 +0400

    Error page was created

--- MiniOPF/src/main/java/servlets/customerServlets/CustomerOptionsServlet.java
+++ MiniOPF/src/main/java/servlets/customerServlets/CustomerOptionsServlet.java
@@ -1,21 +1,22 @@
 package servlets.customerServlets;
 
-import controller.Controller;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import controller.exceptions.IllegalLoginOrPasswordException;
 import controller.exceptions.InvalidInputDataException;
 import controller.exceptions.PasswordMismatchException;
 import model.Model;
-import model.ModelDB;
 import model.ModelFactory;
 import model.database.exceptions.DataNotUpdatedWarning;
 import model.dto.CustomerDTO;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
-import java.io.IOException;
-
-@WebServlet (name = "CustomerOptionsServlet", value = "/CustomerOptionsServlet")
+@WebServlet(name = "CustomerOptionsServlet", value = "/CustomerOptionsServlet")
 public class CustomerOptionsServlet extends HttpServlet
 {
     @Override
@@ -24,46 +25,65 @@ public class CustomerOptionsServlet extends HttpServlet
         CustomerDTO customer = (CustomerDTO) request.getSession().getAttribute("currentUser");
         Model model = ModelFactory.getModel();
 
-        if("click".equals(request.getParameter("changePassword"))){
+        if ("click".equals(request.getParameter("changePassword")))
+        {
             String oldPass = request.getParameter("oldPassword");
-            String newPassword1= request.getParameter("newPassword");
-            String newPassword2= request.getParameter("newPassword2");
-            if (customer.getPassword().equals(oldPass) ){
-                if(newPassword1.equals(newPassword2)){
+            String newPassword1 = request.getParameter("newPassword");
+            String newPassword2 = request.getParameter("newPassword2");
+            if (customer.getPassword().equals(oldPass))
+            {
+                if (newPassword1.equals(newPassword2))
+                {
                     customer.setPassword(newPassword1);
-                }else {
-                    throw new IllegalLoginOrPasswordException();
                 }
-            }else {
-                throw  new PasswordMismatchException();
+                else
+                {
+                    throw new IllegalLoginOrPasswordException("Passwords are different");
+                }
             }
-        }else if("click".equals(request.getParameter("save"))){
+            else
+            {
+                throw new PasswordMismatchException("Wrong old password");
+            }
+        }
+        else if ("click".equals(request.getParameter("save")))
+        {
             String name = request.getParameter("name");
             String lastName = request.getParameter("lastName");
             String address = request.getParameter("address");
 
-            if(!"".equals(name)){
+            if (!"".equals(name))
+            {
                 customer.setFirstName(name);
-            }else {
-                throw new InvalidInputDataException();
-            }if(!"".equals(lastName)){
-                customer.setLastName(lastName);
-            }else {
-                throw new InvalidInputDataException();
-            }if(!"".equals(address)){
-                customer.setAddress(address);
-            }else {
-                throw new InvalidInputDataException();
             }
+            else
+            {
+                throw new InvalidInputDataException();
+            } if (!"".equals(lastName))
+        {
+            customer.setLastName(lastName);
+        }
+        else
+        {
+            throw new InvalidInputDataException();
+        } if (!"".equals(address))
+        {
+            customer.setAddress(address);
+        }
+        else
+        {
+            throw new InvalidInputDataException();
+        }
         }
         try
         {
             model.updateCustomer(customer);
-        } catch (DataNotUpdatedWarning dataNotUpdatedWarning)
+        }
+        catch (DataNotUpdatedWarning dataNotUpdatedWarning)
         {
             dataNotUpdatedWarning.printStackTrace();
         }
-        request.getSession().setAttribute("currentUser",customer);
+        request.getSession().setAttribute("currentUser", customer);
         response.sendRedirect("/CustomerNavigationServlet?ref=options");
     }
 }
